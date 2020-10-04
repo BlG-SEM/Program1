6f9b50aefe304cc6b17ccc6d293e5283c411f7af
BlG-SEM <71176268+BlG-SEM@users.noreply.github.com>
2020-10-04 14:31:27 +0300

    Update Task1.java

--- src/Task1.java
+++ src/Task1.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        double s = input();
-        output(s);
-    }
-    private static double input() {
-        Scanner Scanner = new Scanner(System.in);
         System.out.println("Введите радиусы окружностей (от меньшей к большей)");
-        double r1 = Scanner.nextDouble();
-        double r2 = Scanner.nextDouble();
-        double r3 = Scanner.nextDouble();
-        return calculateS(r1, r2, r3);
+        double r1 = setRadius();
+        double r2 = setRadius();
+        double r3 = setRadius();
+        double s = calculateS(r1, r2, r3);
+        printS(s);
+    }
+    private static double setRadius() {
+        Scanner Scanner = new Scanner(System.in);
+        double r = Scanner.nextDouble()
+        return r;
     }
     private static double calculateS1(double r1) {
         return 0.25 * Math.PI * r1 * r1;
@@ -29,7 +30,7 @@ public class Task1 {
     private static double calculateS(double r1, double r2, double r3) {
         return calculateS1(r1) + calculateS2(r1, r2) + calculateS3(r2, r3) + calculateS4(r3);
     }
-    private static void output(double s){
+    private static void printS(double s){
         Locale.setDefault(Locale.ROOT);
         System.out.printf("Площадь заштрихованной фигуры равна = %1$.3f", s);
     }
